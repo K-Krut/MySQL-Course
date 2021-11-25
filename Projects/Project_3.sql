0f2d90fa9557fc9ff4bf1584bbf338cab6588080
Katia Krut <90685950+K-Krut@users.noreply.github.com>
2021-11-25 14:33:44 +0200

    Update Project_3.sql

--- Projects/Project_3.sql
+++ Projects/Project_3.sql
@@ -2,7 +2,7 @@ USE coursework;
 
 SELECT user_id, available_amount FROM Finance
 
--- WHERE available_amount > 50 AND available_amount < 100
+WHERE available_amount > 50 AND available_amount < 100
 -- WHERE available_amount > 60 OR user_id = "Дуб"
 -- WHERE available_amount < 100  AND NOT user_id  = "Дуб"
 -- WHERE available_amount BETWEEN 50 AND 70
